4c41b04b6e101bcf153bb1dc048e4400d188599d
Pherang <pherang@users.noreply.github.com>
2018-05-27 06:31:47 -0400

    initialize project

---
+++ app/routes.py
@@ -0,0 +1,18 @@
+from flask import render_template
+from app import app
+
+@app.route('/')
+@app.route('/index')
+def index():
+    user = {'username': 'Kyoto'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Toronto'
+        },
+        {
+            'author': {'username:' 'Susan'},
+            'body': 'The Avengers movie was ok'
+        }
+    ]
+    return render_template('index.html', title="Superblog", posts=posts, user=user)
