a65a0a10ae90d4939d3d703b8ce3982e38912069
Pherang <pherang@users.noreply.github.com>
2018-05-31 07:11:53 -0400

    User can now edit profile

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,5 @@
 from flask import render_template, flash, redirect, url_for, request
+from werkzeug.urls import url_parse
 
 # Imports the app class was assigned Flask from the app folder
 from app import app, db
@@ -11,8 +12,19 @@ from app.forms import LoginForm, RegistrationForm
 from flask_login import current_user, login_user, logout_user, login_required
 from app.models import User
 
+from datetime import datetime
+
+@app.before_request
+def before_request():
+    # a reference to current_user will open a session to the database.
+    # which is why db.session.add() isn't here.
+    if current_user.is_authenticated:
+        current_user.last_seen = datetime.utcnow()
+        db.session.commit()
+
 @app.route('/')
 @app.route('/index')
+@login_required # this is initialized in app/__init__.py
 def index():
     user = {'username': 'Kyoto'}
     posts = [
@@ -39,6 +51,11 @@ def login():
         # login_user is a Flask-Login function that will
         # set current_user to the user. Saves the session..
         login_user(user, remember=form.remember_me.data)
+        
+        # The @login_required decorator will redirect here and 
+        # add a query string ?next=/index which can be processed to redirect
+        # the user to where they wanted to go. However it can be abused
+        # so it must be parsed.
         next_page = request.args.get('next')
         if not next_page or url_parse(next_page).netloc != '':
             next_page = url_for('index')
@@ -73,3 +90,20 @@ def user(username):
         {'author': user, 'body': 'Test post #2'}
     ]
     return render_template('user.html', user=user, posts=posts)
+
+@app.route('/edit_profile', methods=['GET', 'POST'])
+@login_required
+def edit_profile():
+    form = EditProfileForm()
+    if form.validate_on_submit():
+        current_user.username = form.username.data
+        current_user.about_me = form.about_me.data
+        db.session.commit()
+        flash('Your changes have been saved')
+        return redirect(url_for('edit_profile'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.about_me.data = current_user.about_me
+    return render_template('edit_profile.html', title='Edit Profile',
+                            form=form)
+
