2fa4e48c34a91c16c389e635abcd2ee7eef2e54f
Pherang <pherang@users.noreply.github.com>
2018-06-02 07:27:01 -0400

    Added automated testing suite

--- app/models.py
+++ app/models.py
@@ -58,11 +58,13 @@ class User(UserMixin, db.Model):
         return self.followed.filter(
             followers.c.followed_id == user.id).count() > 0
 
+    # returns users posts as well as followed posts
     def followed_posts(self):
-        return Post.query.join(
+        followed = Post.query.join(
             followers, (followers.c.followed_id == Post.user_id)).filter(
-                followers.c.follower_id == self.id).order_by(
-                    Post.timestamp.desc())
+                followers.c.follower_id == self.id)
+        own = Post.query.filter_by(user_id=self.id)    
+        return followed.union(own).order_by(Post.timestamp.desc())
 
     # We've defined our own repr() method below
     # this allows us to control what is returned when repr() is called.
