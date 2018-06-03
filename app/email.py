12deaf5c633849dc90965af2d3fff4e4de636817
Pherang <pherang@users.noreply.github.com>
2018-06-03 07:48:58 -0400

    Added background thread to process email

--- app/email.py
+++ app/email.py
@@ -2,12 +2,18 @@ from app import app
 from flask_mail import Message
 from app import mail
 from flask import render_template
+from threading import Thread
+
+def async_send_email(app, msg):
+    with app.app_context(): # Here we are creating the application context from the Flask app we passed from Thread below
+        mail.send(msg)
 
 def send_email(subject, sender, recipients, text_body, html_body):
     msg = Message(subject, sender=sender, recipients=recipients)
     msg.body = text_body
     msg.html = html_body
-    mail.send(msg)
+    # The app argument below is the Flask instance
+    Thread(target=async_send_email, args=(app, msg)).start()
 
 def send_password_reset_email(user):
     token = user.get_reset_password_token()
