486760bf0adcdf12a2b69eb9c751a2e669337ac4
Pherang <pherang@users.noreply.github.com>
2018-06-18 14:48:22 -0400

    heroku dep. fixed value too long error

--- app/models.py
+++ app/models.py
@@ -55,7 +55,7 @@ class SearchableMixin(object):
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
-    email = db.Column(db.String(12), index=True, unique=True) 
+    email = db.Column(db.String(24), index=True, unique=True) 
     password_hash = db.Column(db.String(128))
     about_me = db.Column(db.String(140))
     last_seen = db.Column(db.DateTime, default=datetime.utcnow)
